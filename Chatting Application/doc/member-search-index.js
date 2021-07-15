f320bddae8cc5645f4f4d5e62e644b3f7df7cc79
SERAJ KHAN <60543788+its-seraj@users.noreply.github.com>
2021-07-15 18:04:12 +0530

    Delete Chatting Application/doc directory

--- Chatting Application/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Client","l":"actionPerformed(ActionEvent)","url":"actionPerformed(java.awt.event.ActionEvent)"},{"p":"<Unnamed>","c":"Client","l":"main(String[])","url":"main(java.lang.String[])"}]
